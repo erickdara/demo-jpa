6749619b543dbe5b36364407d140fef001e34af7
Erick David Rangel Garcia <erick1rg@gmail.com>
2022-05-13 12:21:38 -0500

    Create JPA Entity Course

---
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -0,0 +1,23 @@
+package com.in28minutes.jpa.hibernate.demo.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+
+}
