3e1660a8bc7d138f5e001a556620787392766d95
Erick David Rangel Garcia <erick1rg@gmail.com>
2022-05-21 11:42:03 -0500

    Entity Manager Methods - refresh

--- src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
@@ -38,24 +38,19 @@ public class CourseRepository {
 
     public void playWithEntityManager(){
 
-        /*Course course = new Course("Web Services in 100 Steps");
-        em.persist(course);
-        course.setName("Web Services in 100 Steps - Updated");*/
         Course course1 = new Course("Web Services in 100 Steps");
         em.persist(course1);
-
-
         Course course2 = new Course("Angular JS Services in 100 Steps");
         em.persist(course2);
 
         em.flush();
 
-       em.clear();
 
         course1.setName("Web Services in 100 Steps - Updated");
-        em.flush();
-
         course2.setName("Angular JS Services in 100 Steps - Updated");
+
+        em.refresh(course1);
+
         em.flush();
     }
 }
