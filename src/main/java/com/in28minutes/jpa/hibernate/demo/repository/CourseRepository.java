13e1dcf335959d57fff6f0e855cdabbe32d5a1a0
Erick David Rangel Garcia <erick1rg@gmail.com>
2022-06-07 23:05:27 -0500

    ManyToOne Mapping - Generilazing Insert

--- src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -19,25 +20,25 @@ public class CourseRepository {
     @Autowired
     EntityManager em;
 
-    public Course findById(Long id){
-        return em.find(Course.class,id);
+    public Course findById(Long id) {
+        return em.find(Course.class, id);
     }
 
-    public Course save(Course course){
-        if(course.getId()==null){
+    public Course save(Course course) {
+        if (course.getId() == null) {
             em.persist(course);
-        }else {
+        } else {
             em.merge(course);
         }
         return course;
     }
 
-    public void deleteById(Long id){
+    public void deleteById(Long id) {
         Course course = findById(id);
         em.remove(course);
     }
 
-    public void playWithEntityManager(){
+    public void playWithEntityManager() {
 
         Course course1 = new Course("Web Services in 100 Steps");
         em.persist(course1);
@@ -46,13 +47,13 @@ public class CourseRepository {
         course2.setName("JPA in 50 Steps - Updated");
     }
 
-    public void addReviewsForCourse() {
+    public void addHarcodedReviewsForCourse() {
         //Get the course 10003
         Course course = findById(10003L);
         logger.info("course.getReviews() -> {}", course.getReviews());
         //Add 2 reviews to it
-        Review review1 = new Review("5","Great Hands-on Stuff.");
-        Review review2 = new Review("5","Hatsoff.");
+        Review review1 = new Review("5", "Great Hands-on Stuff.");
+        Review review2 = new Review("5", "Hatsoff.");
 
         //Setting the relationship
         course.addReview(review1);
@@ -64,5 +65,22 @@ public class CourseRepository {
         //Save it to the database
         em.persist(review1);
         em.persist(review2);
+    }
+
+    public void addReviewsForCourse(Long courseId, List<Review> reviews) {
+        //Get the course 10003
+        Course course = findById(courseId);
+        logger.info("course.getReviews() -> {}", course.getReviews());
+
+        for (Review review : reviews) {
+            //Add 2 reviews to it
+
+            //Setting the relationship
+            course.addReview(review);
+            review.setCourse(course);
+
+            //Save it to the database
+            em.persist(review);
+        }
     }
 }
