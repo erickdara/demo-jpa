87a4ba939fc37c96c551c4e7da926e65164e5b07
Erick David Rangel Garcia <erick1rg@gmail.com>
2022-07-26 12:45:47 -0500

    Criteria Query - Courses like 100 Steps Criteria Query - Courses without Students

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO course(id, name, created_date, last_updated_date) values (10001, 'JPA in 50 Steps',sysdate(),sysdate());
 INSERT INTO course(id, name, created_date, last_updated_date) values (10002, 'JPA in 40 Steps',sysdate(),sysdate());
 INSERT INTO course(id, name, created_date, last_updated_date) values (10003, 'JPA in 30 Steps',sysdate(),sysdate());
-INSERT INTO course(id, name, created_date, last_updated_date) values (10004, 'JPA in 20 Steps',sysdate(),sysdate());
+INSERT INTO course(id, name, created_date, last_updated_date) values (10004, 'JPA in 100 Steps',sysdate(),sysdate());
 
 INSERT INTO passport(id,number ) values (40001,'E123456');
 INSERT INTO passport(id,number ) values (40002,'A123456');
--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CriteriaQueryTest.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CriteriaQueryTest.java
@@ -16,6 +16,7 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class CriteriaQueryTest {
     EntityManager em;
 
     @Test
-    public void jpql_basic(){
+    public void all_courses(){
         //Select c From Course c
 
         //1. Use Criteria Builder to create a Criteria Query returning the
@@ -47,4 +48,51 @@ public class CriteriaQueryTest {
         logger.info("TypedQuery -> {}", resultList);
     }
 
+    @Test
+    public void all_courses_having_100Steps(){
+        //Select c From Course c where name like '%100 Steps'
+
+        //1. Use Criteria Builder to create a Criteria Query returning the
+        //expected result object
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        //2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        //3. Define Predicates etc using Criteria Builder
+        Predicate like100Steps = cb.like(courseRoot.get("name"),"%100 Steps");
+
+        //4. Add Predicates etc to the Criteria Query
+        cq.where(like100Steps);
+
+        //5. Build the TypedQuery using entity manager and criteria query
+        TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+        List<Course> resultList = query.getResultList();
+        logger.info("TypedQuery -> {}", resultList);
     }
+
+    @Test
+    public void all_courses_without_students(){
+        //Select c From Course c where c.students is empty'
+
+        //1. Use Criteria Builder to create a Criteria Query returning the
+        //expected result object
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        //2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        //3. Define Predicates etc using Criteria Builder
+        Predicate studentsIsEmpty = cb.isEmpty(courseRoot.get("students"));
+
+        //4. Add Predicates etc to the Criteria Query
+        cq.where(studentsIsEmpty);
+
+        //5. Build the TypedQuery using entity manager and criteria query
+        TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+        List<Course> resultList = query.getResultList();
+        logger.info("TypedQuery -> {}", resultList);
+    }
+}
